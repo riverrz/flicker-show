6ba0ec5de2ea1d6ca101474d1493c2afc939acd3
shivam123425 <shivam123425@gmail.com>
2022-08-10 20:40:58 +0530

    Add missing indexes

--- server/user-management/src/prisma/migrations/20220809173450_init/migration.sql
+++ server/user-management/src/prisma/migrations/20220809173450_init/migration.sql
@@ -1,4 +1,13 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Profile" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
@@ -7,6 +16,9 @@ CREATE TABLE "Profile" (
 
     CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ server/user-management/src/prisma/migrations/20220809173647_add_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Profile_userId_idx" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE INDEX "Profile_id_idx" ON "Profile"("id");
+
+-- CreateIndex
+CREATE INDEX "User_id_idx" ON "User"("id");
